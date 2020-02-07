3ceefc8ea1ff4a8d4272738d4913f5eb36719f65
Mikołaj Wąsacz <56759408+mwasacz@users.noreply.github.com>
2020-02-07 20:36:50 +0100

    Improve MW LED Cube 4x4x4 exporter

--- LedCubeAnimator/Model/Exporter.cs
+++ LedCubeAnimator/Model/Exporter.cs
@@ -14,97 +14,83 @@ namespace LedCubeAnimator.Model
         {
             using (var bw = new BinaryWriter(File.Open(path, FileMode.Create)))
             {
-                int start = animation.MainGroup.Start;
-                int end = animation.MainGroup.End;
-                int step = 25; // ToDo
-                int endStep = 250; // ToDo
+                //int repeat = animation.MainGroup.RepeatCount; // ToDo
+                int duration = Math.Max(animation.FrameDuration, 1);
+
+                int start = animation.MainGroup.Start * duration;
+                int end = animation.MainGroup.End * duration;
+
+                const int step = 25; // ToDo
+                const int endStep = 150; // ToDo
 
                 byte[] prevBytes = null;
-                int s = 0;
+                int prevT = 0;
 
-                for (int t = start; t <= end; t += step)
+                for (int t = start; t <= end - endStep; t++)
                 {
-                    var voxels = Renderer.Render(animation, t, false);
+                    var voxels = Renderer.Render(animation, t / duration, false);
 
                     byte max = 0;
                     byte[] bytes = new byte[11];
                     for (int z = 0; z < 4; z++)
                     {
-                        byte b = 0;
-                        byte m = 1;
-                        for (int y = 0; y < 4; y++)
-                        {
-                            var v = voxels[0, y, z];
-                            max = Max(max, v);
-                            b |= v == Colors.Black ? (byte)0 : m;
-                            m <<= 1;
-                        }
-                        for (int y = 0; y < 4; y++)
-                        {
-                            var v = voxels[3, y, z];
-                            max = Max(max, v);
-                            b |= v == Colors.Black ? (byte)0 : m;
-                            m <<= 1;
-                        }
+                        byte b;
+                        b = PrepareByteHalf(voxels, 0, z, ref max, 0x01);
+                        b |= PrepareByteHalf(voxels, 3, z, ref max, 0x10);
                         bytes[z] = b;
-                    }
-                    for (int z = 0; z < 4; z++)
-                    {
-                        byte b = 0;
-                        byte m = 1;
-                        for (int y = 0; y < 4; y++)
-                        {
-                            var v = voxels[1, y, z];
-                            max = Max(max, v);
-                            b |= v == Colors.Black ? (byte)0 : m;
-                            m <<= 1;
-                        }
-                        for (int y = 0; y < 4; y++)
-                        {
-                            var v = voxels[2, y, z];
-                            max = Max(max, v);
-                            b |= v == Colors.Black ? (byte)0 : m;
-                            m <<= 1;
-                        }
+
+                        b = PrepareByteHalf(voxels, 1, z, ref max, 0x01);
+                        b |= PrepareByteHalf(voxels, 2, z, ref max, 0x10);
                         bytes[z + 4] = b;
                     }
                     max &= 0xF0;
                     bytes[8] = max;
 
-                    if (t == start || (bytes.SequenceEqual(prevBytes) && s <= 0x0FFF - step))
+                    if (t == start)
                     {
-                        s += step;
+                        prevBytes = bytes;
+                        prevT = t;
+                        t += step - 1;
                     }
-                    else
+                    else if (!bytes.SequenceEqual(prevBytes))
                     {
-                        prevBytes[8] |= (byte)(s >> 8 & 0x0F);
-                        prevBytes[9] = (byte)s;
-                        prevBytes[10] = 0;
+                        WriteBytes(bw, prevBytes, t - prevT, step);
 
-                        bw.Write(prevBytes);
-
-                        s = step;
+                        prevBytes = bytes;
+                        prevT = t;
+                        t += step - 1;
                     }
-
-                    prevBytes = bytes;
                 }
 
-                s = Math.Max(s, endStep);
+                WriteBytes(bw, prevBytes, end - prevT, endStep);
+            }
+        }
 
-                prevBytes[8] |= (byte)(s >> 8);
-                prevBytes[9] = (byte)s;
+        private static byte PrepareByteHalf(Color[,,] voxels, int x, int z, ref byte max, byte m)
+        {
+            byte b = 0;
+            for (int y = 0; y < 4; y++)
+            {
+                var v = voxels[x, y, z];
+                max = Math.Max(max, v.GetBrightness());
+                b |= v == Colors.Black ? (byte)0 : m;
+                m <<= 1;
+            }
+            return b;
+        }
+
+        private static void WriteBytes(BinaryWriter bw, byte[] prevBytes, int d, int step)
+        {
+            byte prev8 = prevBytes[8];
+            for (int l; d > 0; d -= l)
+            {
+                l = d <= 0x0FFF ? d : Math.Min(d - step, 0x0FFF);
+                prevBytes[8] = (byte)((l >> 8 & 0x0F) | prev8);
+                prevBytes[9] = (byte)l;
                 prevBytes[10] = 0;
 
                 bw.Write(prevBytes);
             }
-        }
-
-        private static byte Max(byte b, Color c)
-        {
-            b = Math.Max(b, c.R);
-            b = Math.Max(b, c.G);
-            b = Math.Max(b, c.B);
-            return b;
         }
     }
 }
