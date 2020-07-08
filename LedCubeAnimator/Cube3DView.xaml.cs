ea5646b8998e4447c4fe5226e2b5b3df219806c4
Mikołaj Wąsacz <56759408+mwasacz@users.noreply.github.com>
2020-07-08 15:18:44 +0200

    Use HelixToolkit for 3D view

--- LedCubeAnimator/Cube3DView.xaml.cs
+++ LedCubeAnimator/Cube3DView.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Media.Media3D;
+using HelixToolkit.Wpf;
 
 namespace LedCubeAnimator
 {
@@ -24,17 +25,6 @@ namespace LedCubeAnimator
         public Cube3DView()
         {
             InitializeComponent();
-
-            DefineCamera();
-            DefineLights();
-            //DefineMaterials();
-
-            //CreateModels();
-            //OrderModels();
-
-            ModelVisual3D modelVisual = new ModelVisual3D();
-            modelVisual.Content = _modelGroup;
-            MainViewport.Children.Add(modelVisual);
         }
 
 
@@ -49,16 +39,7 @@ namespace LedCubeAnimator
 
         private static void OnFrameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((Cube3DView)d).OnFrameChanged(e);
 
-        private void OnFrameChanged(DependencyPropertyChangedEventArgs e)
-        {
-            if (Frame == null)
-            {
-                return;
-            }
-
-            CreateModels(Frame);
-            OrderModels();
-        }
+        private void OnFrameChanged(DependencyPropertyChangedEventArgs e) => UpdateSpheres(e.OldValue as Color[,,], e.NewValue as Color[,,]);
 
 
 
@@ -72,260 +53,203 @@ namespace LedCubeAnimator
 
 
 
-        // 3D scene
-        private Model3DGroup _modelGroup = new Model3DGroup();
-        private PerspectiveCamera _camera;
-        private Light[] _lights;
+        private GeometryModel3D[,,] _spheres;
 
-        // Transform applied to camera and lights
-        private Transform3DGroup _transform = new Transform3DGroup();
-
-        // List of spheres
-        private Dictionary<GeometryModel3D, Point3D> _models = new Dictionary<GeometryModel3D, Point3D>();
-
-        // Material for spheres
-        private Dictionary<Color, Material> _materials;
-
-        // Camera position
-        private double _theta;
-        private double _phi;
-        private double _zoom = 400;
-
-        // Mouse position
-        private bool _mouseDown;
-        private Point _mousePos;
+        private Dictionary<Color, Material> _materials = new Dictionary<Color, Material>();
 
 
 
-        private void DefineCamera()
+        private void UpdateSpheres(Color[,,] oldFrame, Color[,,] newFrame)
         {
-            _camera = new PerspectiveCamera();
-            _camera.FieldOfView = 60;
-            _camera.Position = new Point3D(0, 0, _zoom / 100);
+            var oldVoxels = oldFrame ?? new Color[0, 0, 0];
+            var newVoxels = newFrame ?? new Color[0, 0, 0];
 
-            _camera.Transform = _transform;
-
-            MainViewport.Camera = _camera;
-        }
-
-        private void DefineLights()
-        {
-            AmbientLight ambient_light = new AmbientLight(Colors.Gray);
-            DirectionalLight directional_light = new DirectionalLight(Colors.Gray, new Vector3D(-1.0, -3.0, -2.0));
-
-            directional_light.Transform = _transform;
-
-            _lights = new Light[] { ambient_light, directional_light };
-        }
-
-        /*private void DefineMaterials()
-        {
-            //MaterialGroup group = new MaterialGroup();
-            //group.Children.Add(new DiffuseMaterial(new SolidColorBrush(Color.FromRgb(0, 0, 0x80))));
-            //group.Children.Add(new SpecularMaterial(new SolidColorBrush(Color.FromRgb(0, 0, 0x80)), 50));
-            //group.Children.Add(new EmissiveMaterial(new SolidColorBrush(Color.FromRgb(0, 0, 0x64))));
-            //_selectedMaterial = group;
-
-            _selectedMaterial = new DiffuseMaterial(Brushes.Blue);
-            _normalMaterial = new DiffuseMaterial(new SolidColorBrush(Color.FromArgb(0x80, 0xFF, 0xFF, 0xFF)));
-        }*/
-
-        private MeshGeometry3D CreateSphere(double r, Point3D p, int n)
-        {
-            double segmentRad = Math.PI / n;
-            int separators = 2 * n;
-
-            Point3DCollection points = new Point3DCollection();
-            for (int e = -n / 2; e <= n / 2; e++)
+            if (oldVoxels.GetLength(0) != newVoxels.GetLength(0)
+                || oldVoxels.GetLength(1) != newVoxels.GetLength(1)
+                || oldVoxels.GetLength(2) != newVoxels.GetLength(2))
             {
-                double r_e = r * Math.Cos(segmentRad * e);
-                double y_e = r * Math.Sin(segmentRad * e);
-
-                for (int s = 0; s <= separators - 1; s++)
-                {
-                    double z_s = r_e * Math.Sin(segmentRad * s) * -1;
-                    double x_s = r_e * Math.Cos(segmentRad * s);
-                    points.Add(new Point3D(p.X + x_s, p.Y + y_e, p.Z + z_s));
-                }
-            }
-            points.Add(new Point3D(p.X, p.Y + r, p.Z));
-            points.Add(new Point3D(p.X, p.Y - r, p.Z));
-
-            Int32Collection triangleIndices = new Int32Collection();
-            for (int i = 0; i < separators; i++)
-            {
-                triangleIndices.Add(i);
-                triangleIndices.Add(separators * (n + 1) + 1);
-                triangleIndices.Add((i + 1) % separators);
-
-                for (int e = 0; e < n; e++)
-                {
-                    triangleIndices.Add(e * separators + i);
-                    triangleIndices.Add(e * separators + (i + 1) % separators + separators);
-                    triangleIndices.Add(e * separators + i + separators);
-
-                    triangleIndices.Add(e * separators + (i + 1) % separators + separators);
-                    triangleIndices.Add(e * separators + i);
-                    triangleIndices.Add(e * separators + (i + 1) % separators);
-                }
-
-                triangleIndices.Add(n * separators + i);
-                triangleIndices.Add(n * separators + (i + 1) % separators);
-                triangleIndices.Add(separators * (n + 1));
+                CreateSpheres(newVoxels);
             }
 
-            MeshGeometry3D mesh = new MeshGeometry3D();
-            mesh.Positions = points;
-            mesh.TriangleIndices = triangleIndices;
+            UpdateMaterials(newVoxels);
 
-            return mesh;
-        }
-
-        private void CreateModels(Color[,,] voxels)
-        {
-            Dictionary<Point3D, GeometryModel3D> oldModels = _models.ToDictionary(m => m.Value, m => m.Key);
-
-            bool createNew = oldModels.Count != voxels.Length;
-
-            _models = new Dictionary<GeometryModel3D, Point3D>();
-
-            _materials = new Dictionary<Color, Material>();
-            foreach (Color voxel in voxels)
+            for (int x = 0; x < newVoxels.GetLength(0); x++)
             {
-                if (!_materials.ContainsKey(voxel))
+                for (int y = 0; y < newVoxels.GetLength(1); y++)
                 {
-                    Color sphereColor = voxel;
-                    sphereColor.A = (byte)(Math.Min(voxel.R + voxel.G + voxel.B, 255) * 7 / 8 + 32);
-                    _materials.Add(voxel, new DiffuseMaterial(new SolidColorBrush(sphereColor)));
-                }
-            }
-
-            Vector3D center = new Vector3D(0.5 * (voxels.GetLength(0) - 1), 0.5 * (voxels.GetLength(1) - 1), 0.5 * (voxels.GetLength(2) - 1));
-
-            //int n = 1280 / voxels.Length; // ToDo: set n based on voxels.Length
-            int n = 20;
-
-            for (int x = 0; x < voxels.GetLength(0); x++)
-            {
-                for (int y = 0; y < voxels.GetLength(1); y++)
-                {
-                    for (int z = 0; z < voxels.GetLength(2); z++)
+                    for (int z = 0; z < newVoxels.GetLength(2); z++)
                     {
-                        Point3D p = new Point3D(x, y, z);
-
-                        GeometryModel3D model;
-                        if (createNew)
-                        {
-                            MeshGeometry3D mesh = CreateSphere(0.25, p - center, n);
-                            model = new GeometryModel3D(mesh, _materials[voxels[x, y, z]]);
-                        }
-                        else
-                        {
-                            model = oldModels[p];
-                            model.Material = _materials[voxels[x, y, z]];
-                        }
-
-                        _models.Add(model, p);
+                        var material = _materials[newVoxels[x, y, z]];
+                        _spheres[x, y, z].Material = material;
+                        //_spheres[x, y, z].BackMaterial = material;
                     }
                 }
             }
         }
 
-        private void OrderModels()
+        private void CreateSpheres(Color[,,] newVoxels)
         {
-            Point3D pos = _transform.Transform(_camera.Position);
-            _modelGroup.Children = new Model3DCollection(
-                _lights.AsEnumerable<Model3D>().Concat(
-                _models.OrderByDescending(m => (pos - m.Value).Length).Select(m => m.Key)));
-        }
+            SortingVisual3D.Children.Clear();
 
+            _spheres = new GeometryModel3D[newVoxels.GetLength(0), newVoxels.GetLength(1), newVoxels.GetLength(2)];
 
+            var mesh = CreateSphereMesh(newVoxels.GetLength(0) * newVoxels.GetLength(1) * newVoxels.GetLength(2));
 
-        public void HitTest(Point mousePos)
-        {
-            HitTestResult result = VisualTreeHelper.HitTest(MainViewport, mousePos);
+            Point3D offset = new Point3D(
+                0.5 * (newVoxels.GetLength(0) - 1),
+                0.5 * (newVoxels.GetLength(1) - 1),
+                0.5 * (newVoxels.GetLength(2) - 1));
 
-            if (result is RayMeshGeometry3DHitTestResult meshResult)
+            for (int x = 0; x < newVoxels.GetLength(0); x++)
             {
-                GeometryModel3D model = (GeometryModel3D)meshResult.ModelHit;
+                for (int y = 0; y < newVoxels.GetLength(1); y++)
+                {
+                    for (int z = 0; z < newVoxels.GetLength(2); z++)
+                    {
+                        var model = new GeometryModel3D
+                        {
+                            Geometry = mesh,
+                            Transform = new TranslateTransform3D(new Point3D(x, y, z) - offset)
+                        };
 
-                Command?.Execute(_models[model]);
+                        _spheres[x, y, z] = model;
+
+                        SortingVisual3D.Children.Add(new ModelVisual3D { Content = model });
+                    }
+                }
             }
         }
 
-
-
-        public void Zoom()
+        private static MeshGeometry3D CreateSphereMesh(int sphereCount)
         {
-            // For zooming we simply change the Z-position of the camera
-            _camera.Position = new Point3D(_camera.Position.X, _camera.Position.Y, _zoom / 100);
-            OrderModels();
+            int div = (int)Math.Sqrt((double)65536 / sphereCount);
+
+            //var builder = new MeshBuilder(false, false);
+            //builder.AddSphere(new Point3D(0, 0, 0), 0.25, 2 * div, div);
+            //return builder.ToMesh();
+
+            return CreateSphereMesh(new Point3D(0, 0, 0), 0.25, div);
         }
 
-        private void Rotate()
+        private static MeshGeometry3D CreateSphereMesh(Point3D center, double radius, int div)
         {
-            // Clamp phi (pitch) between -90 and 90 to avoid 'going upside down'
-            if (_phi < -90) _phi = -90;
-            if (_phi > 90) _phi = 90;
+            double d = Math.PI / div;
 
-            Vector3D thetaAxis = new Vector3D(0, 1, 0);
-            Vector3D phiAxis = new Vector3D(-1, 0, 0);
+            Point3DCollection positions = new Point3DCollection();
 
-            _transform.Children.Clear();
-            _transform.Children.Add(new RotateTransform3D(new AxisAngleRotation3D(-phiAxis, _phi)));
-            _transform.Children.Add(new RotateTransform3D(new AxisAngleRotation3D(-thetaAxis, _theta)));
+            positions.Add(new Point3D(center.X, center.Y, center.Z + radius));
 
-            // Z ordering
-            OrderModels();
-        }
-
-
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Right)
+            for (int i = 1; i < div; i++)
             {
-                _mouseDown = true;
+                double phi = i * d;
 
-                _mousePos = e.GetPosition(MainViewport);
+                for (int j = 0; j < 2 * div; j++)
+                {
+                    double theta = j * d;
+
+                    double x = Math.Cos(theta) * Math.Sin(phi);
+                    double y = Math.Sin(theta) * Math.Sin(phi);
+                    double z = Math.Cos(phi);
+
+                    positions.Add(new Point3D(center.X + radius * x, center.Y + radius * y, center.Z + radius * z));
+                }
             }
-            else if (e.ChangedButton == MouseButton.Left)
+
+            positions.Add(new Point3D(center.X, center.Y, center.Z - radius));
+
+            Int32Collection triangleIndices = new Int32Collection();
+
+            int last = (div - 1) * 2 * div + 1;
+
+            for (int j = 0; j < 2 * div; j++)
+            {
+                int next = j == 2 * div - 1 ? 1 - 2 * div : 1;
+
+                triangleIndices.Add(j + 1 + next);
+                triangleIndices.Add(0);
+                triangleIndices.Add(j + 1);
+
+                for (int i = 0; i < div - 2; i++)
+                {
+                    int ij = i * 2 * div + j + 1;
+
+                    triangleIndices.Add(ij);
+                    triangleIndices.Add(ij + 2 * div + next);
+                    triangleIndices.Add(ij + next);
+
+                    triangleIndices.Add(ij + 2 * div + next);
+                    triangleIndices.Add(ij);
+                    triangleIndices.Add(ij + 2 * div);
+                }
+
+                triangleIndices.Add(last - 2 * div + j);
+                triangleIndices.Add(last);
+                triangleIndices.Add(last - 2 * div + j + next);
+            }
+
+            return new MeshGeometry3D
+            {
+                Positions = positions,
+                TriangleIndices = triangleIndices
+            };
+        }
+
+        private void UpdateMaterials(Color[,,] newVoxels)
+        {
+            var newMaterials = new Dictionary<Color, Material>();
+
+            foreach (Color voxel in newVoxels)
+            {
+                if (!newMaterials.ContainsKey(voxel))
+                {
+                    if (!_materials.TryGetValue(voxel, out var material))
+                    {
+                        var sphereColor = new Color
+                        {
+                            A = (byte)(Math.Max(Math.Max(voxel.R, voxel.G), voxel.B) * 7 / 8 + 32),
+                            R = voxel.R,
+                            G = voxel.G,
+                            B = voxel.B
+                        };
+                        material = new DiffuseMaterial(new SolidColorBrush(sphereColor));
+                    }
+                    newMaterials.Add(voxel, material);
+                }
+            }
+
+            _materials = newMaterials;
+        }
+
+
+
+        private void MainViewport_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
             {
                 HitTest(e.GetPosition(MainViewport));
             }
         }
 
-        private void Window_MouseMove(object sender, MouseEventArgs e)
+        public void HitTest(Point mousePos)
         {
-            if (_mouseDown)
+            var model = MainViewport.Viewport.FindHits(mousePos).FirstOrDefault()?.Model;
+
+            if (model != null)
             {
-                Point newPos = e.GetPosition(MainViewport);
-
-                _theta += (newPos.X - _mousePos.X) / 3;
-                _phi += (_mousePos.Y - newPos.Y) / 3;
-
-                _mousePos = newPos;
-
-                Rotate();
+                for (int x = 0; x < _spheres.GetLength(0); x++)
+                {
+                    for (int y = 0; y < _spheres.GetLength(1); y++)
+                    {
+                        for (int z = 0; z < _spheres.GetLength(2); z++)
+                        {
+                            if (_spheres[x, y, z] == model)
+                            {
+                                Command?.Execute(new Point3D(x, y, z));
+                            }
+                        }
+                    }
+                }
             }
-        }
-
-        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Right)
-            {
-                _mouseDown = false;
-            }
-        }
-
-        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            _zoom -= e.Delta;
-            if (_zoom < 0)
-            {
-                _zoom = 0;
-            }
-            Zoom();
         }
     }
 }
