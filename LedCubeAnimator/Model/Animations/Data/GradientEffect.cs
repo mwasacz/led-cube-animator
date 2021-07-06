69d276965d6fa1652b8872200e1346b6e4378e4b
Mikołaj Wąsacz <56759408+mwasacz@users.noreply.github.com>
2021-07-06 17:49:44 +0200

    Use HsvColor in GradientEffect

--- LedCubeAnimator/Model/Animations/Data/GradientEffect.cs
+++ LedCubeAnimator/Model/Animations/Data/GradientEffect.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LedCubeAnimator.Utils;
+using System;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
 
@@ -26,75 +27,19 @@ namespace LedCubeAnimator.Model.Animations.Data
             }
             else
             {
-                ColorToHSV(From, out double fromH, out double fromS, out double fromV);
-                ColorToHSV(To, out double toH, out double toS, out double toV);
+                var from = From.ToHsv();
+                var to = To.ToHsv();
 
-                double d1 = toH - fromH + (toH < fromH ? 360 : 0);
-                double d2 = fromH - toH + (fromH <= toH ? 360 : 0);
+                double d1 = to.H - from.H + (to.H < from.H ? 360 : 0);
+                double d2 = from.H - to.H + (from.H <= to.H ? 360 : 0);
                 double d = (d1 < d2) == (ColorInterpolation == ColorInterpolation.HSV) ? d1 : -d2;
-                double h = (fromH + d * fraction + 360) % 360;
 
-                return ColorFromHSV(
-                    h,
-                    fromS + (toS - fromS) * fraction,
-                    fromV + (toV - fromV) * fraction);
-            }
-        }
+                double h = (from.H + d * fraction + 360) % 360;
+                double s = from.S + (to.S - from.S) * fraction;
+                double v = from.V + (to.V - from.V) * fraction;
 
-        private static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
-        {
-            double r = (double)color.R / 255;
-            double g = (double)color.G / 255;
-            double b = (double)color.B / 255;
-
-            double max = Math.Max(Math.Max(r, g), b);
-            double min = Math.Min(Math.Min(r, g), b);
-            double delta = max - min;
-
-            if (delta == 0)
-            {
-                hue = 0;
-            }
-            else if (r == max)
-            {
-                hue = ((g - b) / delta + 6) % 6 * 60;
-            }
-            else if (g == max)
-            {
-                hue = ((b - r) / delta + 2) * 60;
-            }
-            else
-            {
-                hue = ((r - g) / delta + 4) * 60;
-            }
-            saturation = max == 0 ? 0 : delta / max;
-            value = max;
-        }
-
-        private static Color ColorFromHSV(double hue, double saturation, double value)
-        {
-            int i = (int)(hue / 60);
-            double f = hue / 60 - i;
-
-            byte v = (byte)(255 * value);
-            byte p = (byte)(255 * value * (1 - saturation));
-            byte q = (byte)(255 * value * (1 - saturation * f));
-            byte t = (byte)(255 * value * (1 - saturation * (1 - f)));
-
-            switch (i)
-            {
-                case 1:
-                    return new Color { A = 255, R = q, G = v, B = p };
-                case 2:
-                    return new Color { A = 255, R = p, G = v, B = t };
-                case 3:
-                    return new Color { A = 255, R = p, G = q, B = v };
-                case 4:
-                    return new Color { A = 255, R = t, G = p, B = v };
-                case 5:
-                    return new Color { A = 255, R = v, G = p, B = q };
-                default:
-                    return new Color { A = 255, R = v, G = t, B = p };
+                var result = new HsvColor(h, s, v);
+                return result.ToRgb();
             }
         }
     }
